9308abc7b71f310a0d2e0c78fb8403d1c77da19e
jarry-kuang <60911333+jarry-kuang@users.noreply.github.com>
2020-06-09 01:52:50 +0800

    Add files via upload

---
+++ 3.0/main file/movement.m
@@ -0,0 +1,45 @@
+classdef movement< handle
+    
+    properties
+        qMatrix
+    end
+    
+    methods(Static)
+        function object = movement()
+        end
+        
+        function move2knowlocation(robot,q2)            %location(know) -->location(know)
+            robotname=robot.robot.name;
+            steps = 5;
+            qMatrix = jtraj(robot.robot.getpos,q2,steps);           %   Quintic Polynomial
+           robot.robot.plot(qMatrix,'trail','r')  
+          
+            drawnow()                  %colour error         s = lspb(0,1,steps);                                             	% First, create the scalar function
+%             qMatrix = nan(steps,4);                                             % Create memory allocation for variables
+%             for i = 1:steps
+%                 qMatrix(i,:) = (1-s(i))*robot.robot.getpos + s(i)*q2;           % Generate interpolated joint angles
+%                 animate(robot.robot,qMatrix(i,:));
+%                 drawnow();
+%             end 
+        end
+          
+        function move2unknowlocation(robot,location)
+            steps = 10;
+            q1=robot.robot.getpos();
+            q2 = robot.robot.ikcon(location,q1);
+          qMatrix = jtraj(robot.robot.getpos,q2,steps);           %   Quintic Polynomial
+          robot.robot.plot(qMatrix,'trail','r') 
+          drawnow()                  %colour error
+%             s = lspb(0,1,steps); 
+%             qMatrix = nan(steps,4);                                             % Create memory allocation for variables
+%             for i = 1:steps
+%                 qMatrix(i,:) = (1-s(i))*robot.robot.getpos + s(i)*q2;           % Generate interpolated joint angles
+%                 animate(robot.robot,qMatrix(i,:));
+%                 drawnow();
+            end    
+        end  
+    end
+
+
+
+
